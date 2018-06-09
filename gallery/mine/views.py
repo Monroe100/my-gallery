0d62d33c71566323228c8eb5a5280238768fe1a9
Monroe100 <michelle1hunnid@gmail.com>
2018-06-09 10:22:56 +0300

    create error handler in views.py

--- gallery/mine/views.py
+++ gallery/mine/views.py
@@ -33,8 +33,14 @@ def convert_dates(dates):
     return day
 
 def past_days_image(request,past_date):
+    try:
+
         # Converts data from the string Url
     date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
     day = convert_dates(date)
 
     html = f'''
