396991521f3a1b8860499cea8fd0668d603c8750
Monroe100 <michelle1hunnid@gmail.com>
2018-06-10 11:52:49 +0300

    create setUp test in Category class

--- mine/tests.py
+++ mine/tests.py
@@ -26,3 +26,26 @@ class LocationTestClass(TestCase):
         self.test_location.delete_locations()
         locations = Location.objects.all()
         self.assertTrue(len(locations) < 1)
+
+
+
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.test = Category(category="Food")
+
+    def test_instance(self):
+        self.asserTrue(isinstance(self.test, Image))
+
+    # def test_saving_category(self):
+    #     self.test.save_category()
+    #     images = Category.objects.all()
+    #     self.assertTrue(len(images) > 0)
+
+    # def test_deleting_category(self):
+    #     self.test = Category(category="Food")
+    #     self.test.save_category()
+    #     self.test.delete_locations()
+    #     locationss = Category.objects.all()
+    #     self.assertTrue(len(locationss) < 1)
+
