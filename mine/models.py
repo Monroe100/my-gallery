c62f0fef30e14605412f077d4f7b850f396e4feb
Monroe100 <michelle1hunnid@gmail.com>
2018-06-10 10:02:59 +0300

    update settings.py

--- mine/models.py
+++ mine/models.py
@@ -23,9 +23,22 @@ class Location(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=30, blank=True)
 
-    
-     def save_category(self):
+
+    def save_category(self):
         self.save()
 
     def delete_category(self):
         self.delete()
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='gallery/', blank=True)
+    image_url = models.TextField(blank=True)
+    image_name = models.CharField(max_length=30, blank=True)
+    description = models.TextField(max_length=100, blank=True)
+    category = models.ManyToManyField(Category, blank=True)
+    post_date = models.DateTimeField(auto_now=True)
+    location = models.ForeignKey(Location)
