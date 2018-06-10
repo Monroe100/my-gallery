ed5c684041c85783e59c1b24023d16758d28c943
Monroe100 <michelle1hunnid@gmail.com>
2018-06-10 15:02:22 +0300

    register the location class

--- mine/admin.py
+++ mine/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import Image,Location,Category
 # Register your models here.
+
+admin.site.register(Location)
+# admin.site.register(Article)
+# admin.site.register(tags)
