33f646513285d12f91488c04454034b885b09927
Monroe100 <michelle1hunnid@gmail.com>
2018-06-10 17:23:18 +0300

    update mine/urls.py

--- mine/urls.py
+++ mine/urls.py
@@ -9,3 +9,6 @@ urlpatterns = [
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_image,name = 'pastImage'),
     url(r'^search/', views.search_results, name='search_results'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
