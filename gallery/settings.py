8eaa443160c4c7197ea73cab393d499f3e661b4d
Monroe100 <michelle1hunnid@gmail.com>
2018-06-12 16:32:59 +0300

    deploying to heroku

--- gallery/settings.py
+++ gallery/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DBNAME'),
-        'USER':config('USER'),
+        'USER':config('DBUSER'),
         'PASSWORD':config('DBPASSWORD')
    }
 }
